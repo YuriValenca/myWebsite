f7a844ed469e35676881baf07371340a9c5c5933
Yuri Valença <yvc@cin.ufpe.br>
2023-04-10 17:24:02 -0300

    style(PokeConsult): :nail_care: Style the PokeDetails component
    Signed-off-by: Yuri Valença <yvc@cin.ufpe.br>

--- src/PokeConsult/PokemonDetails/index.tsx
+++ src/PokeConsult/PokemonDetails/index.tsx
@@ -5,34 +5,44 @@ import { fetchPokemon } from "../store/actions/pokemonActions";
 import { RootState, useAppDispatch } from "../store/store";
 import {
   DetailsContainer,
-  PokemonName,
+  InputButtonContainer,
+  PokemonInput,
+  PokeButton,
+  PokemonDetailBody,
   PokemonImage,
+  DetailsStatsContainer,
+  TextContainer,
+  PokemonName,
+  PokemonStats,
+  Stat,
   PokemonTypes,
   Type,
-  PokemonHeight,
-  PokemonWeight,
-  PokemonInput,
-  InputButtonContainer,
-  PokeButton,
+  ErrorContainer,
 } from './style';
 
 interface PokemonDetailsTypes {
+  id?: number;
   name?: string;
   imageUrl?: string;
   types?: string[];
+  stats?: number[];
   height?: number;
   weight?: number;
 }
 
 const PokemonDetails = ({
+  id,
   name,
   imageUrl,
   types,
+  stats,
   height,
   weight,
 }: PokemonDetailsTypes) => {
   const dispatch = useAppDispatch();
   const [pokemonName, setPokemonName] = useState('');
+
+  const isLoading = useSelector((state: RootState) => state.pokemon.isLoading);
 
   const error = useSelector((state: RootState) => state.pokemon.error);
 
@@ -44,35 +54,90 @@ const PokemonDetails = ({
     dispatch(fetchPokemon(pokemonName) as unknown as AnyAction);
   }
 
+  const dualTypePokemon = () => {
+    return types && types.length > 1 ? true : false;
+  }
+
   return (
-    <DetailsContainer>
-      <InputButtonContainer>
-        <PokemonInput
-          type="text"
-          placeholder="Search for a pokemon"
-          value={pokemonName}
-          onChange={handleInputChange}
-          onSubmit={handleSearch}
-        />
-        <PokeButton onClick={handleSearch}>Search</PokeButton>
-      </InputButtonContainer>
-      {!error ?
-        <>
-          <PokemonName>{name}</PokemonName>
-          <PokemonImage
-            src={imageUrl}
-            alt={name}
-          />
-          <PokemonTypes>{
-            types?.map((type: string) => (
-              <Type>{type}</Type>
-            ))
-          }</PokemonTypes>
-          <PokemonHeight>Height: {height ? height / 10 : 1}m</PokemonHeight>
-          <PokemonWeight>Weight: {weight ? weight / 10 : 1}kg</PokemonWeight>
-        </>
-        : 'Pokemon not found'}
-    </DetailsContainer>
+    <>
+      {isLoading ?
+        <div
+          style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '200px' }}
+        >
+          <img height="40px" width="40px" src="https://i.gifer.com/origin/34/34338d26023e5515f6cc8969aa027bca_w200.gif" alt="loading" />
+        </div>
+        :
+        <DetailsContainer>
+          <InputButtonContainer>
+            <PokemonInput
+              type="text"
+              placeholder="Search for a pokemon"
+              value={pokemonName}
+              onChange={handleInputChange}
+              onSubmit={handleSearch}
+              onKeyDown={(event) => {
+                if (event.key === 'Enter') {
+                  handleSearch();
+                }
+              }}
+            />
+            <PokeButton onClick={handleSearch}>Search</PokeButton>
+          </InputButtonContainer>
+          {!error ?
+            <PokemonDetailBody>
+              <PokemonImage
+                src={imageUrl}
+                alt={name}
+              />
+              <DetailsStatsContainer>
+                <TextContainer>
+                  <PokemonName>{name && (name?.charAt(0).toUpperCase() + name?.slice(1))}, #{id}</PokemonName>
+                  <PokemonStats>
+                    <Stat>
+                      HP: {stats ? stats[0] : 1}
+                    </Stat>
+                    <Stat>
+                      Attack: {stats ? stats[1] : 1}
+                    </Stat>
+                    <Stat>
+                      Defense: {stats ? stats[2] : 1}
+                    </Stat>
+                    <Stat>
+                      SpAtk: {stats ? stats[3] : 1}
+                    </Stat>
+                    <Stat>
+                      SpDef: {stats ? stats[4] : 1}
+                    </Stat>
+                    <Stat>
+                      Speed: {stats ? stats[5] : 1}
+                    </Stat>
+                  </PokemonStats>
+                  Height: {height ? height / 10 : 1}m
+                  <br />
+                  Weight: {weight ? weight / 10 : 1}kg
+                </TextContainer>
+                <PokemonTypes
+                  dualType={dualTypePokemon()}
+                >{
+                    types?.map((type: string) => (
+                      <Type
+                        type={type.charAt(0).toUpperCase() + type?.slice(1)}
+                      >
+                        {type.charAt(0).toUpperCase() + type.slice(1)}
+                      </Type>
+                    ))
+                  }</PokemonTypes>
+              </DetailsStatsContainer>
+            </PokemonDetailBody>
+            :
+            <ErrorContainer>
+              <p>Sorry :&#40; we couldn't find any results that match your input</p>
+              <img src="https://media.tenor.com/KJzbxvVG5qwAAAAC/detective-pikachu-investigation.gif" alt="error" />
+            </ErrorContainer>
+          }
+        </DetailsContainer>
+      }
+    </>
   )
 
 };
--- src/PokeConsult/PokemonDetails/style.ts
+++ src/PokeConsult/PokemonDetails/style.ts
@@ -1,18 +1,86 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-export const DetailsContainer = styled.div``;
+interface DualTypeTypes {
+  dualType: boolean;
+}
 
-export const PokemonName = styled.p``;
+interface TypeColorTypes {
+  type: string;
+}
 
-export const PokemonImage = styled.img``;
+const typeStyles = {
+  Normal: css`
+    background-color: #A8A878;
+  `,
+  Fire: css`
+    background-color: #F08030;
+  `,
+  Water: css`
+    background-color: #6890F0;
+  `,
+  Grass: css`
+    background-color: #78C850;
+  `,
+  Electric: css`
+    background-color: #F8D030;
+  `,
+  Ice: css`
+    background-color: #98D8D8;
+  `,
+  Fighting: css`
+    background-color: #C03028;
+    color: #fff;
+  `,
+  Poison: css`
+    background-color: #A040A0;
+    color: #fff;
+  `,
+  Ground: css`
+    background-color: #E0C068;
+  `,
+  Flying: css`
+    background-color: #A890F0;
+  `,
+  Psychic: css`
+    background-color: #F85888;
+  `,
+  Bug: css`
+    background-color: #A8B820;
+  `,
+  Rock: css`
+    background-color: #B8A038;
+  `,
+  Ghost: css`
+    background-color: #705898;
+    color: #fff;
+  `,
+  Dragon: css`
+    background-color: #7038F8;
+    color: #fff;
+  `,
+  Dark: css`
+    background-color: #705848;
+    color: #fff;
+  `,
+  Steel: css`
+    background-color: #B8B8D0;
+  `,
+  Fairy: css`
+    background-color: #EE99AC;
+  `,
+};
 
-export const PokemonTypes = styled.div``;
+export const DetailsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 16px;
+  height: calc(100vh - 65px);
+`;
 
-export const Type = styled.p``;
-
-export const PokemonHeight = styled.p``;
-
-export const PokemonWeight = styled.p``;
+export const InputButtonContainer = styled.div`
+  margin-left: 35px;
+`;
 
 export const PokemonInput = styled.input`
   width: 450px;
@@ -21,16 +89,79 @@ export const PokemonInput = styled.input`
   background-color: transparent;
   border: 0;
   border-bottom: 3px solid #333;
+  font-size: 18px;
 
   &:focus {
     outline: none;
   }
 `;
 
-export const InputButtonContainer = styled.div`
-  margin-left: 35px;
-`;
-
 export const PokeButton = styled.button`
   margin-left: 15px;
+`;
+
+export const PokemonDetailBody = styled.div`
+  display: flex;
+  margin-top: 36px;
+  font-size: 20px;
+`;
+
+export const PokemonImage = styled.img`
+  width: 400px;
+  height: auto;
+  background-color: #ECECEC;
+  border-radius: 6px;
+  margin-right: 16px;
+`;
+
+export const DetailsStatsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 300px;
+`;
+
+export const TextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-family: flexoregular;
+  background-color: #30a7d7;
+  border-radius: 24px;
+  padding: 8px 16px;
+`;
+
+export const PokemonName = styled.p`
+  font-size: 32px;
+  font-weight: 600;
+`;
+
+export const PokemonStats = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  margin: 12px 0;
+`;
+
+export const Stat = styled.p``;
+
+export const PokemonTypes = styled.div<DualTypeTypes>`
+  display: flex;
+  justify-content: ${(props) => (props.dualType ? "space-evenly" : "center")};
+  font-family: flexoregular;
+  margin-top: 8px;
+`;
+
+export const Type = styled.div<TypeColorTypes>`
+  padding: 4px 14px;
+  border-radius: 4px;
+  font-weight: bold;
+  ${({ type }) => typeStyles[type as keyof typeof typeStyles]};
+`;
+
+export const ErrorContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 36px;
+  font-family: flexoregular;
 `;
