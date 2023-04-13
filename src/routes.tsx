a02bba08387683bd87b5ebc4299525c7749cc9ea
Yuri Valença <yvc@cin.ufpe.br>
2023-04-12 22:24:46 -0300

    feat(FavPokemon): :sparkles: Add FavPokemon component to routes
    Signed-off-by: Yuri Valença <yvc@cin.ufpe.br>

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,14 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import App from './App';
 import PokeConsult from './PokeConsult/pokeConsult';
+import FavPokemon from './FavPokemon/favPokemon';
 
 const RoutesComponent = () => {
   return (
     <Routes>
       <Route path="/" element={App()} />
       <Route path="PokeConsult/pokeConsult" element={PokeConsult()} />
+      <Route path="FavPokemon/favPokemon" element={FavPokemon()} />
     </Routes>
   );
 }
