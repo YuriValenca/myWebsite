54b6072f47383c5499ebf4bd0cb3d4af416b6dff
Yuri Valença <yvc@cin.ufpe.br>
2023-10-13 21:17:55 -0300

    :sparkles: Create pokemon Autocomplete and fetch images and names to select a pokemon
    Signed-off-by: Yuri Valença <yvc@cin.ufpe.br>

--- src/projects/PokemonGrid/components/Grid/index.tsx
+++ src/projects/PokemonGrid/components/Grid/index.tsx
@@ -1,10 +1,19 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Pokemon } from "../../store/types";
 import {
   GridContainer,
-  GridItem
+  GridItem,
+  ModalContainer,
+  ModalTitle,
+  AutocompletePokemonList,
+  AutocompletePokemonItem,
+  AutocompletePokemonImage,
+  NameAndButtonContainer,
+  PokemonSuggestionName,
+  SelectPokemonButton,
 } from "./style";
 import Modal from "../../../../components/Modal";
+import Input from "../../../../components/Input";
 
 interface GridTypes {
   pokemonData: Pokemon[];
@@ -14,9 +23,49 @@ const Grid = ({
   pokemonData,
 }: GridTypes) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [inputValue, setInputValue] = useState('');
+  const [suggestions, setSuggestions] = useState<Pokemon[]>([]);
+  const autocompleteRef = useRef<HTMLUListElement>(null);
 
   const openModal = () => {
     setIsModalOpen(true);
+  };
+
+  const handleSearch = (searchValue: string) => {
+    console.log(searchValue);
+  }
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setInputValue(value);
+
+    if (value.length >= 4) {
+      const filteredSuggestions = pokemonData.filter(pokemon =>
+        pokemon.name.toLowerCase().includes(value.toLowerCase())
+      );
+      setSuggestions(filteredSuggestions);
+    } else {
+      setSuggestions([]);
+    }
+  };
+
+  const formatPokemonName = (name: string) => {
+    return name
+      .replace(/-/g, ' ')
+      .split(' ')
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(' ');
+  }
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      handleSearch(inputValue);
+      setSuggestions([]);
+    }
+  };
+
+  const handleAutocompleteItemClick = (selectedSuggestion: string) => {
+    console.log(selectedSuggestion);
   };
 
   return (
@@ -29,10 +78,49 @@ const Grid = ({
       ))}
       {isModalOpen && (
         <Modal
-          onClose={() => setIsModalOpen(false)}
+          onClose={() => {
+            setIsModalOpen(false)
+            setInputValue('');
+            setSuggestions([]);
+          }}
           width="30%"
+          position="top"
         >
-          <h1>Modal content</h1>
+          <ModalContainer>
+            <ModalTitle>Make your guess</ModalTitle>
+            <Input
+              placeholder="Type your guess here"
+              value={inputValue}
+              onChange={handleInputChange}
+              onKeyDown={handleKeyDown}
+            />
+            {suggestions.length > 0 && (
+              <AutocompletePokemonList
+                ref={autocompleteRef}
+              >
+                {suggestions.map((suggestion, index) => (
+                  <AutocompletePokemonItem
+                    key={index}
+                    onClick={() => handleAutocompleteItemClick(suggestion.name)}
+                  >
+                    <AutocompletePokemonImage
+                      src={suggestion.image}
+                      alt={suggestion.name}
+                    />
+                    <NameAndButtonContainer>
+                      <PokemonSuggestionName>
+                        {formatPokemonName(suggestion.name)}
+                      </PokemonSuggestionName>
+                      <SelectPokemonButton>
+                        Select
+                      </SelectPokemonButton>
+                    </NameAndButtonContainer>
+                  </AutocompletePokemonItem>
+                ))}
+              </AutocompletePokemonList>
+            )}
+          </ModalContainer>
+
         </Modal>
       )}
     </GridContainer>
