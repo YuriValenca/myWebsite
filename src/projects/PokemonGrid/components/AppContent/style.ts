7e81b863e562f8e61426ea0c4d13c1f52e9d3f28
Yuri Valença <yvc@cin.ufpe.br>
2023-10-17 14:01:38 -0300

    :sparkles: Create and style the types for the challenge
    Signed-off-by: Yuri Valença <yvc@cin.ufpe.br>

--- src/projects/PokemonGrid/components/AppContent/style.ts
+++ src/projects/PokemonGrid/components/AppContent/style.ts
@@ -1,7 +1,12 @@
 import styled from "styled-components";
 
-export const Title = styled.h1`
-  font-family: "pokemon_classicregular";
+export const AppContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+  background-color: #E8E9EB;
 `;
 
 export const PokemonGridContainer = styled.div`
@@ -10,4 +15,47 @@ export const PokemonGridContainer = styled.div`
   flex-wrap: wrap;
   justify-content: center;
   align-items: center;
+  max-width: 800px;
 `;
+
+export const Title = styled.h1`
+  font-family: "pokemon_classicregular";
+  font-size: 36px;
+  margin-bottom: 24px;
+`;
+
+export const ChallengesRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  width: 100%;
+`;
+
+export const PokeballImage = styled.img`
+  width: 72px;
+  height: 72px;
+  margin-left: -18px;
+`;
+
+export const GridAndTypesContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+export const ChallengesColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  width: 100%;
+`;
+
+export const Type = styled.div`
+  font-family: "pokemon_classicregular";
+  font-size: 18px;
+  margin-right: 12px;
+`;
+
